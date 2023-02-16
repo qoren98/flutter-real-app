6bb36fbb39a1241d62877b9ec3785c062e2a9803
qoren98 <qoren98@gmail.com>
2023-02-16 14:59:23 +0900

    Start

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const _App());
+}
+
+class _App extends StatelessWidget {
+  const _App();
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}
